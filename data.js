89da36ece42280074363945b284d3a124b81fae3
Pinz1234 <luthfikeren545@gmail.com>
2025-12-24 19:13:57 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const scripts = [
+  {
+    name: "Launcher MD",
+    desc: "Bot WhatsApp gratis, ringan, cocok pemula Dengan Fitur 300+.",
+    link: "https://release-assets.githubusercontent.com/github-production-release-asset/1120159085/d1f6dc9b-b288-445b-81ed-ebb65d5585f6?sp=r&sv=2018-11-09&sr=b&spr=https&se=2025-12-24T12%3A35%3A17Z&rscd=attachment%3B+filename%3DLaucher.MD.zip&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2025-12-24T11%3A34%3A45Z&ske=2025-12-24T12%3A35%3A17Z&sks=b&skv=2018-11-09&sig=akg9it%2FTWcX81P3GcjfhSikwUw9lutYSbL6KOWHkSsk%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc2NjU3NjM4NSwibmJmIjoxNzY2NTc2MDg1LCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.q7_JIwgpAfqpMCHfbJFUCrmqijR9voS_vT8EjqVC2rU&response-content-disposition=attachment%3B%20filename%3DLaucher.MD.zip&response-content-type=application%2Foctet-stream"
+  }
+];
